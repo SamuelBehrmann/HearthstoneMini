39e3bc4cd2355e789b6d3e98958169131246abed
SamuelBehrmann <samuel.behrmann@yahoo.de>
2022-03-30 10:45:42 +0200

    update healthpoint

---
+++ src/main/scala/model/healthpoints.scala
@@ -0,0 +1,8 @@
+package model
+
+case class Healthpoints(value: Int):
+    val max: Int
+    def increase(amount: Int): Healthpoints= copy(value = value - amount)
+    def decrease(amount: Int): Healthpoints = copy(value = value - amount)
+    def isEmpty(): Boolean = value <= 0
+    def setVal(amount: Int): Healthpoints = copy(value = amount)

345acc51e68b860c3760d87ecb32262fbc55f507
SamuelBehrmann <samuel.behrmann@yahoo.de>
2022-04-01 08:31:59 +0200

    test/manaTest

--- src/test/scala/MySuite.scala
+++ src/test/scala/MySuite.scala
@@ -1,9 +1,20 @@
 // For more information on writing tests, see
 // https://scalameta.org/munit/docs/getting-started.html
-class MySuite extends munit.FunSuite {
-  test("example test that succeeds") {
-    val obtained = 42
-    val expected = 42
-    assertEquals(obtained, expected)
+package hearstonemini.src.test.scala
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatest.matchers.should.Mathers._
+import src.main.scala.model.mana.scala
+
+class HearthstoneminiTest extends WordSpec with Matchers {
+  "Mana" when {
+    "not set any value " should {
+      val emptyMana = Mana(0)
+      "have Value 0" in {
+        emptyMana.value should be(0)
+      }
+      "not be set" in {
+        emptyMana.isEmpty should be(false)
+      }
+    }
   }
 }

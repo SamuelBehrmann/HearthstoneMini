c3a46debd8fc469b03a576413275c67e5dc8a399
JOPInATOR <janaminger@googlemail.com>
2022-04-06 15:14:33 +0200

    started card class

--- src/main/scala/model/card.scala
+++ src/main/scala/model/card.scala
@@ -1,4 +1,33 @@
 package model
-case class Card() {
+
+
+abstract class Card(name: String, manaCost: Int, attValue: Int, defenseValue: Int, effect: String, rarity: Int) {
+
+  def setName(name: String): Unit
+  def setManaCost(amount: Int): Mana
+  def setAttack(amount: Int): Unit
+  def setDefense(amount: Int): Unit
+  def setEffect(beschreibung: String): Unit
+  def setRarity(rarity: Int): Unit
+
+
+
+  override def toString: String = {
+    name + "\n" + "mana cost: " + manaCost + "\n" + "attack: " + attValue + "\n" +
+      "defense: " + defenseValue + "\n" + "rarity: " + rarity + "\n" + "effect: " + effect
+  }
+
+
 
 }
+
+// enum für rarities
+object rarities extends Enumeration {
+  type rarity = Value
+
+  val gewöhnlich, selten, episch, legendär = Value
+}
+
+
+
+

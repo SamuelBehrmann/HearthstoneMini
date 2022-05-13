1865b151654af43520c133687067f002ccc0d3db
SamuelBehrmann <samuel.behrmann@yahoo.de>
2022-05-13 09:12:46 +0200

    Fixed Active Player not beeing part of the field

--- src/main/scala/model/field.scala
+++ src/main/scala/model/field.scala
@@ -13,22 +13,32 @@ object Field{
     val standartFieldHeight: Int = (standartFieldBarHeight + standartGameBarHeight + standartMenueBarHeight) * 2 + Field.offset
 }
 
-case class Field(matrix: Matrix[String] = new Matrix[String](Field.standartFieldHeight, Field.standartFieldWidth, " "), slotNum: Int = Field.standartSlotNum, players: List[Player] = List[Player](new Player(id = 1), new Player(id = 2))) {
-    def this(size: Int, player1: String, player2: String) = this(new Matrix[String](Field.standartFieldHeight, Field.standartSlotWidth * size, " "), size, players =  List[Player](new Player(name = player1, id = 1), new Player(name = player2, id = 2)))
-    def this(size: Int) = this(new Matrix[String](Field.standartFieldHeight, Field.standartSlotWidth * size, " "), size, players =  List[Player](new Player(id = 1), new Player(id = 2)))
+case class Field(matrix: Matrix[String] = new Matrix[String](Field.standartFieldHeight, Field.standartFieldWidth, " "), 
+    slotNum: Int = Field.standartSlotNum, 
+    players: List[Player] = List[Player](new Player(id = 1), new Player(id = 2))) {
+    // The active player is the Player in position 0 of the list
+    def this(size: Int, player1: String, player2: String) = this(new Matrix[String](Field.standartFieldHeight, Field.standartSlotWidth * size, " "), 
+        size, 
+        players =  List[Player](new Player(name = player1, id = 1), new Player(name = player2, id = 2)))
+    def this(size: Int) = this(new Matrix[String](Field.standartFieldHeight, Field.standartSlotWidth * size, " "), 
+        size, 
+        players =  List[Player](new Player(id = 1), new Player(id = 2)))
 
-    def placeCard(playerID: Int, handSlot: Int, fieldSlot: Int): Field = copy(players = players.updated(playerID, players(playerID).placeCard(handSlot ,fieldSlot)))
-    def drawCard(playerID: Int): Field = copy(players = players.updated(playerID, players(playerID).drawCard()))
-    def destroyCard(playerID: Int, slot: Int): Field = copy(players = players.updated(playerID, players(playerID).destroyCard(slot)))
-    def reduceHp(playerID: Int, amount: Int): Field = copy(players = players.updated(playerID, players(playerID).reduceHp(amount)))
-    def increaseHp(playerID: Int, amount: Int): Field = copy(players = players.updated(playerID, players(playerID).increaseHp(amount)))
-    def reduceMana(playerID: Int, amount: Int): Field = copy(players = players.updated(playerID, players(playerID).reduceMana(amount)))
-    def increaseMana(playerID: Int, amount: Int): Field = copy(players = players.updated(playerID, players(playerID).increaseMana(amount)))
+    def placeCard( handSlot: Int, fieldSlot: Int): Field = copy(players = players.updated(0, players(0).placeCard(handSlot ,fieldSlot)))
+    def drawCard(): Field = copy(players = players.updated(0, players(0).drawCard()))
+    def destroyCard( slot: Int): Field = copy(players = players.updated(0, players(0).destroyCard(slot)))
+    def reduceHp( amount: Int): Field = copy(players = players.updated(0, players(0).reduceHp(amount)))
+    def increaseHp( amount: Int): Field = copy(players = players.updated(0, players(0).increaseHp(amount)))
+    def reduceMana( amount: Int): Field = copy(players = players.updated(0, players(0).reduceMana(amount)))
+    def increaseMana( amount: Int): Field = copy(players = players.updated(0, players(0).increaseMana(amount)))
     def setPlayerNames(p1: String, p2: String): Field = copy(players = players.updated(0, players(0).setName(p1)).updated(1, players(1).setName(p2)))
+    def switchPlayer() = copy(players = players.reverse)
+    def getPlayerById(id: Int) = players.filter(_.id == id)(0)
+    def getActivePlayer() = players(0)
     def toMatrix(): Matrix[String] = matrix
     .updateMatrix(0, 0, List[String]("-" * Field.standartFieldWidth))
-    .updateMatrixWithMatrix(Field.offset, 0, players(0).toMatrix())
-    .updateMatrixWithMatrix(Field.offset + Field.standartMenueBarHeight + Field.standartGameBarHeight + Field.standartFieldBarHeight, 0, players(1).toMatrix())
+    .updateMatrixWithMatrix(Field.offset, 0, getPlayerById(1).toMatrix())
+    .updateMatrixWithMatrix(Field.offset + Field.standartMenueBarHeight + Field.standartGameBarHeight + Field.standartFieldBarHeight, 0, getPlayerById(2).toMatrix())
 
     override def toString() = toMatrix().rows.map(_.mkString("|","","|\n")).mkString
 }

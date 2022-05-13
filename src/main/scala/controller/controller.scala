dd15a8dcdcc7442a06e7e3fafbc3c2290b9ac9af
SamuelBehrmann <samuel.behrmann@yahoo.de>
2022-05-13 08:35:37 +0200

    small controller fix

--- src/main/scala/controller/controller.scala
+++ src/main/scala/controller/controller.scala
@@ -7,28 +7,14 @@ import model.Move
 import java.lang.System.exit
 case class Controller(var field: Field) extends Observable {
      var player = 0
-
-     def placeCard(move: Move) = {
-          field.placeCard(player, move.handSlot, move.fieldSlot)
-     }
-     def destroyCard(move: Move) = {
-          field.destroyCard(player, move.fieldSlot)
-     }
-     def drawCard(move: Move) = {
-          field.drawCard(player)
-     }
-     def reduceHp(move: Move) = {
-          field.reduceHp(player, move.amount)
-     }
-     def increaseHp(move: Move) = {
-          field.increaseHp(player, move.amount)
-     }
-     def reduceMana(move: Move) = {
-          field.reduceMana(player, move.amount)
-     }
-     def increaseMana(move: Move) = {
-          field.increaseMana(player, move.amount)
-     }
+     def placeCard(move: Move) = field.placeCard(player, move.handSlot, move.fieldSlot)
+     def destroyCard(move: Move) = field.destroyCard(player, move.fieldSlot)
+     def drawCard(move: Move) = field.drawCard(player)
+     def reduceHp(move: Move) = field.reduceHp(player, move.amount) 
+     def increaseHp(move: Move) = field.increaseHp(player, move.amount)
+     def reduceMana(move: Move) = field.reduceMana(player, move.amount)
+     def increaseMana(move: Move) = field.increaseMana(player, move.amount)
+     def setPlayerNames(move: Move) = field.setPlayerNames(move.p1,move.p2)
      def switchPlayer(move: Move) = {
           if(player == 0) then player = 1 else player = 0 
           field
@@ -37,14 +23,9 @@ case class Controller(var field: Field) extends Observable {
           exit(0)
           field
      }
-     def setPlayerNames(move: Move) = {
-          field.setPlayerNames(move.p1,move.p2)
-     }
-
      def doAndPublish(doThis: Move => Field, move: Move ) = {
           field = doThis(move)
           notifyObservers
      }
-
      override def toString() = field.toString
 }

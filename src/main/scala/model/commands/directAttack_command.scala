f1c0305a337d75be744507237c97a4005e6a8e87
SamuelBehrmann <samuel.behrmann@yahoo.de>
2022-05-20 15:23:27 +0200

    fixed direct Attack

--- src/main/scala/model/commands/directAttack_command.scala
+++ src/main/scala/model/commands/directAttack_command.scala
@@ -9,13 +9,14 @@ import model.Card
 class DirectAttackCommand(controller: Controller, move: Move) extends Command {
   var memento: Field = controller.field
   override def doStep: Unit = {
-    if (controller.field.players(0).fieldbar.cardArea.slot(move.fieldSlotActive).isDefined) then
+    if (controller.field.players(0).fieldbar.cardArea.slot(move.fieldSlotActive).isDefined) then 
       {
-        if (!controller.field.players(1).fieldbar.cardArea.row.contains(Some[Card])) then
+        if !(controller.field.players(1).fieldbar.cardArea.row.count(_.isDefined) > 0 ) then {
           memento = controller.field
           controller.field = controller.field.reduceHp(1, controller.field.players(0).fieldbar.cardArea.slot(move.fieldSlotActive).get.attValue)
           if (controller.field.players(0).gamebar.hp.isEmpty() || controller.field.players(1).gamebar.hp.isEmpty()) then controller.gameState =  GameState.WIN
-      }
+        } 
+    }   
   }
 
 

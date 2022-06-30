aab1a9886172b9ce3e14d7894a1e9c1bf4e5e875
SamuelBehrmann <samuel.behrmann@yahoo.de>
2022-06-30 17:53:15 +0200

    added run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-sbt run
+ip=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
+xhost + $ip
+docker run --rm -e DISPLAY=$ip:0 -v /tmp/.X11-unix:/tmp/.X11-unix -it hearthstonemini:gui

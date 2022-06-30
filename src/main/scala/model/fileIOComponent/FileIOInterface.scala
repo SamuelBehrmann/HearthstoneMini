0b2998556d64e775ccc0e9d302607eb5142bd0ec
SamuelBehrmann <samuel.behrmann@yahoo.de>
2022-06-30 17:08:23 +0200

    override

--- src/main/scala/model/fileIOComponent/FileIOInterface.scala
+++ src/main/scala/model/fileIOComponent/FileIOInterface.scala
@@ -3,7 +3,7 @@ package model.fileIOComponent
 import model.fieldComponent.FieldInterface
 import model.fieldComponent.fieldImpl.Field
 
-
+//hello
 trait FileIOInterface {
   def load: FieldInterface
   def save(field: FieldInterface): Unit
